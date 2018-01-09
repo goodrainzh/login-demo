7bee833e98d42fbc8caa89fb74649e0e5c32a10f
zhengh <zhengh@goodrain.com>
2018-01-09 11:29:26 +0800

    no message

---
+++ target/classes/net/goodrain/smples/LoginServlet.java
@@ -0,0 +1,34 @@
+package net.goodrain.smples;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	public LoginServlet() {
+		super();
+	}
+	public void destroy() {
+		super.destroy(); // Just puts "destroy" string in log
+		// Put your code here
+	}
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+	}
+	public void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		HttpSession session = request.getSession();
+		session.setAttribute("username",username);
+		session.setAttribute("password",password);
+		response.sendRedirect( request.getContextPath() + "/index2.jsp");
+	}
+	public void init() throws ServletException {
+	}
+
+}
